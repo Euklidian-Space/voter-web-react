fabc7e3a1fc540847db090338f0f323ead10f412
Gabriel <gabriel.grover@aofl.com>
2018-01-02 14:04:25 -0800

    returning errors with password key instead of password_hash as key

--- src/reducers/session/session_selector.js
+++ src/reducers/session/session_selector.js
@@ -1,3 +1,12 @@
 export function getRegistrationErrs({ session }) {
-	return session.errors.registration_errs;
+	const errs = session.errors.registration_errs;
+	if (!errs) return null;
+	return Object.keys(errs).reduce((result, key) => {
+		if (key === "password_hash")
+			result["password"] = errs["password_hash"];
+		else
+			result[key] = errs[key];
+
+		return result;
+	}, {});
 }

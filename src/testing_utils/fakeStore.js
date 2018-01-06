c46528e1da94d38c1dbe5511054ab3820b787380
Gabriel <gabriel.grover@aofl.com>
2018-01-06 08:58:48 -0800

    testing util for deep freezing objects to ensure immutability

--- src/testing_utils/fakeStore.js
+++ src/testing_utils/fakeStore.js
@@ -1,5 +1,5 @@
 
-import { shallow } from 'enzyme';
+import { shallow, mount } from 'enzyme';
 import configureMockstore from 'redux-mock-store';
 import thunk from 'redux-thunk'
 
@@ -16,4 +16,11 @@ export function shallowWithStore(component, store) {
 
 export function shallowNoStore(component) {
 	return shallow(component);
+}
+
+export function mountWithStore(component, store) {
+	const context = {
+		store
+	};
+	return mount(store, { context });
 }

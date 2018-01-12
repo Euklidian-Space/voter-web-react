5f09083bb80290ffe9e0b4df06fcb559e094fde7
Gabriel <gabriel.grover@aofl.com>
2018-01-11 16:50:13 -0800

    test for form submit

--- src/components/LoginForm/__tests__/LoginForm.test.js
+++ src/components/LoginForm/__tests__/LoginForm.test.js
@@ -2,44 +2,104 @@ import React from 'react';
 import TextField from 'material-ui/TextField';
 import RaisedButton from 'material-ui/RaisedButton';
 import Dialog from 'material-ui/Dialog';
-import { shallowWithStore, mockStore } from '../../../testing_utils/fakeStore';
-
-import LoginForm from '../LoginForm';
+import FlatButton from 'material-ui/FlatButton';
+import { shallowWithStore, mockStore, shallowNoStore } from '../../../testing_utils/mounts';
+import { mount } from 'enzyme';
+import LoginForm, { LoginForm as NoStoreLoginForm } from '../LoginForm';
 import { login } from '../../../actions/session';
 
 let store;
-
+let wrapper;
 describe("LoginForm", () => {
-	store = mockStore({
-		session: { errors: { login_errs: {"detail": null} } }
-	});
-	let wrapper;
 
-	beforeEach(() => {
-		wrapper = shallowWithStore(<LoginForm />, store);
-	});
+	afterEach(() => { store = null; wrapper = null });
 
 	it("should render without crashing", () => {
+		store = mockStore({
+			session: { errors: { login_errs: {"detail": null} } }
+		});
+		wrapper = shallowWithStore(<LoginForm />, store);
 		expect(wrapper).toBeTruthy();
 	});
 
-	it("should not display a dialog box if no login errors are present in redux state", () => {
-		expect(wrapper.dive().find(Dialog).props().open).toBe(false);
-	});
+	describe("Form component", () => {
 
-	it("should display a dialog box when errors are present in redux state", () => {
-		store = mockStore({
-			session: {
-				errors: {
-					login_errs: { detail: "Unauthorized" }
-				}
-			}
+		it("should exist", () => {
+			store = mockStore({
+				session: { errors: { login_errs: null } }
+			});
+			wrapper = shallowWithStore(<LoginForm />, store);
+			expect(wrapper.dive().find("Form").length).toBe(1);
 		});
 
-		wrapper = shallowWithStore(<LoginForm />, store);
+		it("should have two fields name Email and Password", () => {
+			store = mockStore({
+				session: { errors: { login_errs: null } }
+			});
+			wrapper = shallowWithStore(<LoginForm />, store);
+			expect(wrapper.dive().find("Form").props().fields.length).toBe(2);
+			expect(wrapper.dive().find("Form").props().fields).toEqual(["Email", "Password"]);
+		});
 
-		expect(wrapper.dive().find(Dialog).props().open).toBe(true);
+		it("should have an onClickHandler", () => {
+			store = mockStore({
+				session: { errors: { login_errs: null } }
+			});
+			wrapper = shallowWithStore(<LoginForm />, store);
+			expect(wrapper.dive().find("Form").props().onClickHandler).toBeDefined();
+		});
+
 	});
 
+	describe("Dialog Box", () => {
+
+		it("should not display if there are no login errors are present in redux state", () => {
+			store = mockStore({session: {errors: {login_errs: null}}});
+			wrapper = shallowWithStore(<LoginForm />, store);
+			expect(wrapper.dive().find(Dialog).props().open).toBe(false);
+		});
+
+		it("should display when errors are present in redux state", () => {
+			store = mockStore({
+				session: {
+					errors: {
+						login_errs: { detail: "Unauthorized" }
+					}
+				}
+			});
+			wrapper = shallowWithStore(<LoginForm />, store);
+			expect(wrapper.dive().find(Dialog).props().open).toBe(true);
+		});
+
+		describe("Dissmiss Button", () => {
+			it("should exist", () => {
+				store = mockStore({
+					session: {
+						errors: {
+							login_errs: { detail: "Unauthorized" }
+						}
+					}
+				});
+				wrapper = shallowWithStore(<LoginForm />, store);
+				let dialog = wrapper.dive().find("Dialog");
+				expect(dialog.props().actions[0].props.label).toBe("Dismiss");
+			});
+
+			it("should have text saying: Incorrect email or password.", () => {
+				store = mockStore({
+					session: {
+						errors: {
+							login_errs: { detail: "Unauthorized" }
+						}
+					}
+				});
+				wrapper = shallowWithStore(<LoginForm />, store);
+				let dialog = wrapper.dive().find("Dialog");
+				expect(dialog.children().get(0)).toBe("Incorrect email or password");
+			});
+
+		});
+
+	});
 
 });

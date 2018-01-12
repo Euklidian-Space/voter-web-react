15f2bcdaaae0fd5c400864249898e69bae03ae9f
Gabriel <gabriel.grover@aofl.com>
2018-01-11 16:49:07 -0800

    implemented form

--- src/components/LoginForm/LoginForm.js
+++ src/components/LoginForm/LoginForm.js
@@ -4,27 +4,40 @@ import TextField from 'material-ui/TextField';
 import RaisedButton from 'material-ui/RaisedButton';
 import Dialog from 'material-ui/Dialog';
 import FlatButton from 'material-ui/FlatButton';
-import { login } from '../../actions/session';
+import { login, clearLoginErrs } from '../../actions/session';
 import { getLoginErrs } from "../../reducers/session/session_selector";
+import Form from '../Form';
 
-class LoginForm extends Component {
+
+export class LoginForm extends Component {
+	onDismiss = () => {
+		console.log("onDismiss!");
+		this.props.clearLoginErrs();
+	}
+
+	onClickHandler = data => {
+		this.props.login(data);
+	}
+
 	render() {
 		const actions = [
 				<FlatButton
-					label="cancel"
+					label="Dismiss"
 					primary={true}
-					onClick={() => null}
+					onClick={this.onDismiss}
 				/>
 		];
+		const fields = ["Email", "Password"] ;
 		return (
 			<div>
+				<Form fields={fields} onClickHandler={this.onClickHandler}/>
 				<Dialog
 					actions={actions}
 					modal={false}
 					open={this.props.errors ? true : false}
-					onRequestClose={() => null}
+					onRequestClose={this.onDismiss}
 				>
-					Hello
+					Incorrect email or password
 				</Dialog>
 			</div>
 		);
@@ -37,4 +50,4 @@ const mapStateToProps = state => {
 	};
 };
 
-export default connect(mapStateToProps, { login })(LoginForm);
+export default connect(mapStateToProps, { login, clearLoginErrs })(LoginForm);
